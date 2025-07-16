5a9e3cd29a8f8eefea7c1e7a575e7f2284301680
Huntress77 <Hjones12074@gmail.com>
2025-07-16 12:03:56 +1200

    Multiplayer, Work!

--- untitled-space-game-ultra-deluxe/Scripts/Global/online.gd
+++ untitled-space-game-ultra-deluxe/Scripts/Global/online.gd
@@ -10,7 +10,16 @@ var port: int = 20277
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(IP.get_local_addresses()[5])
+	if OS.has_feature("windows"):
+		if OS.has_environment("COMPUTERNAME"):
+			hostip =  IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1)
+	elif OS.has_feature("x11"):
+		if OS.has_environment("HOSTNAME"):
+			hostip =  IP.resolve_hostname(str(OS.get_environment("HOSTNAME")),1)
+	elif OS.has_feature("OSX"):
+		if OS.has_environment("HOSTNAME"):
+			hostip =  IP.resolve_hostname(str(OS.get_environment("HOSTNAME")),1)
+	print(hostip)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
