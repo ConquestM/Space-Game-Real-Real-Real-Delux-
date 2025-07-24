7f94e3b94e97259bc95dc03377cc189a7e9b2a2d
Huntress77 <Hjones12074@gmail.com>
2025-07-24 17:13:56 +1200

    Crew UI

---
+++ untitled-space-game-ultra-deluxe/Scripts/UI/Choice Stuff/choices.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+@export var parent: Control
+var scale_float: float = 1
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_scale = lerp(global_scale, Vector2(scale_float, scale_float), delta * 15)
+	
+	if parent.pos_no != 4 and self == parent.get_child(parent.pos_no):
+		scale_float = 1.15
+		if Input.is_action_just_pressed("Player_1_Interact"):
+			print(name)
+	else:
+		scale_float = 0.85
