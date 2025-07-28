03ba06aef5180a86a5792dcdb46793c4b1ea9360
ConquestM <chirsxue123@gmail.com>
2025-07-28 14:41:26 +1200

    working on jump

--- untitled-space-game-ultra-deluxe/Scripts/Enemies/test_enemy.gd
+++ untitled-space-game-ultra-deluxe/Scripts/Enemies/test_enemy.gd
@@ -4,8 +4,12 @@ extends CharacterBody3D
 @export var nav_agent: Node
 const SPEED: int = 4 
 
-func _process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	velocity = Vector3.ZERO
 	nav_agent.set_target_position(player.global_transform.origin)
 	velocity = (nav_agent.get_next_path_position() - global_transform.origin).normalized() * SPEED
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	if (player.global_transform.origin - global_transform.origin) < 10:
+		print("jump")
