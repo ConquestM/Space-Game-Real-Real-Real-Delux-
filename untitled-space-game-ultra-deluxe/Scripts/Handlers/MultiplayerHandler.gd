a06f9ddea987176fbc4026bbd89a0e9fd871ed4a
Huntress77 <Hjones12074@gmail.com>
2025-08-15 13:12:05 +1200

    multishiplayer
    yaeh

--- untitled-space-game-ultra-deluxe/Scripts/Handlers/MultiplayerHandler.gd
+++ untitled-space-game-ultra-deluxe/Scripts/Handlers/MultiplayerHandler.gd
@@ -1,11 +1,35 @@
 extends Node3D
 
+@export var player_scene: PackedScene
+# Multiplayer Stuff
+var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if not global.multiplayer_on:
+		var player = player_scene.instantiate()
+		add_child(player, true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Host a game via inputed port
+	if Online.hosting:
+		Online.hosting = false
+		peer.create_server(Online.port)
+		multiplayer.multiplayer_peer = peer
+		multiplayer.peer_connected.connect(_add_player)
+		_add_player()
+	# Join a game via an ip address and port
+	if Online.joining:
+		Online.joining = false
+		peer.create_client(Online.inputedip, Online.port)
+		multiplayer.multiplayer_peer = peer
+
+
+func _add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	add_child(player, true)
+	player.position.y += 1
